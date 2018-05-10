77cc3abee5666b7912e1d619de1be84da83cc300
praingeard <paul.raingeard@gmail.com>
2018-05-10 19:08:30 +0200

    test(formatter): New test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -14,13 +14,32 @@ var expect = chai.expect;
 // Defines a Mocha test suite to group tests of similar kind together
 describe("Global Extension Tests", function () {
     context("Formatting comand", function () {
-        it("Should respond to command call", async () => {
+        it("Should format a simple file", async () => {
             // Creating the relative path to find the test file
             const testFilesPath = path.join(__dirname, "..", "..", "test_files");
             // Simple test file
             const uri = vscode.Uri.file(path.join(testFilesPath, "hw-toform.s"));
             // Read the expected file
             let expectedFileContents = fs.readFileSync(path.join(testFilesPath, "hw-exp.s"), 'utf8');
+            // Opens the file in the editor
+            await vscode.window.showTextDocument(uri);
+            let editor = vscode.window.activeTextEditor;
+            // tslint:disable-next-line:no-unused-expression
+            expect(editor).to.not.be.undefined;
+            if (editor) {
+                // Editor openned
+                // Call the formatting command
+                await vscode.commands.executeCommand("editor.action.formatDocument");
+                expect(editor.document.getText()).to.be.equal(expectedFileContents);
+            }
+        });
+        it("Should format another simple file with sprite", async () => {
+            // Creating the relative path to find the test file
+            const testFilesPath = path.join(__dirname, "..", "..", "test_files");
+            // Simple test file
+            const uri = vscode.Uri.file(path.join(testFilesPath, "hw2-toform.s"));
+            // Read the expected file
+            let expectedFileContents = fs.readFileSync(path.join(testFilesPath, "hw2-exp.s"), 'utf8');
             // Opens the file in the editor
             await vscode.window.showTextDocument(uri);
             let editor = vscode.window.activeTextEditor;
