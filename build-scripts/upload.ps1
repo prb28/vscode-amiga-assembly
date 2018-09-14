2a70c5f42302132302bd1f3f19e4671ff564e991
praingeard <paul.raingeard@gmail.com>
2018-09-15 01:32:32 +0200

    chore(appveyor): finally ?

--- build-scripts/upload.ps1
+++ build-scripts/upload.ps1
@@ -1,3 +1,2 @@
 $wc = New-Object 'System.Net.WebClient'
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path 'test-report*')[0])
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path 'test-report*')[1])
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\test-report.xml))
