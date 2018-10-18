1b22b2b1bca0b3336fb09ab3b70e1a2cfba10999
Pierre Chalamet <pierre@chalamet.net>
2018-10-18 16:02:49 +0200

    option to change working directory of emulator

--- src/runFsUAENoDebug.ts
+++ src/runFsUAENoDebug.ts
@@ -28,6 +28,8 @@ export interface LaunchRequestArguments extends DebugProtocol.LaunchRequestArgum
 	buildWorkspace?: boolean;
 	/** emulator program */
 	emulator?: string;
+	/** emulator configuration */
+	workingdir?: string;
 	/** Emulator options */
 	options: Array<string>;
 }
@@ -125,12 +127,13 @@ export class RunFsUAENoDebugSession extends DebugSession {
 	public startEmulator(args: LaunchRequestArguments): Promise<void> {
 		logger.warn("Starting emulator: " + args.emulator);
 		const emulatorExe = args.emulator;
+		const workingdir = args.workingdir || null;
 		if (emulatorExe) {
 			// Is the emeulator exe present in the filesystem ?
 			if (this.checkEmulator(emulatorExe)) {
 				return new Promise(async (resolve, reject) => {
 					this.cancellationTokenSource = new CancellationTokenSource();
-					this.executor.runTool(args.options, null, "warning", true, emulatorExe, null, true, null, this.cancellationTokenSource.token).then(() => {
+					this.executor.runTool(args.options, workingdir, "warning", true, emulatorExe, null, true, null, this.cancellationTokenSource.token).then(() => {
 						this.sendEvent(new TerminatedEvent());
 						resolve();
 					}).catch((err) => {
