7b0395307f3ac2ffb4976ddeceb974c8172d2371
praingeard <paul.raingeard@gmail.com>
2019-06-27 19:16:02 +0200

    feat(#73): first example

---
+++ test_files/sources/withtabs.s
@@ -0,0 +1,14 @@
+FW_InitCopperBplPtrs:
+	subq.w	#1,d0			;-1 for dbf
+	ext.l	d2				;Make d2 safe for longword addition
+
+.makecl:
+	swap	d1				;Swap high & low words
+	move.w	d1,2(a0)		;High ptr
+	swap	d1				;Swap high & low words
+	move.w	d1,6(a0)		;Low ptr
+	addq.l	#8,a0			;Next set of ptrs
+	add.l	d2,d1			;Next bitplane
+	dbf	d0,.makecl
+
+	rts
