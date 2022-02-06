d694a872f89fe772453a0cb752613e5b108e34a3
Paul Raingeard <paul.raingeard@gmail.com>
2022-02-07 00:13:15 +0100

    chore(build): removing coverage

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -2,21 +2,8 @@
 
 import * as path from 'path';
 import * as Mocha from 'mocha';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const NYC = require('nyc');
 import * as glob from 'glob';
 import * as paths from "path";
-
-const enableCoverage = process.env.DISABLE_COVERAGE !== "true";
-
-// Simulates the recommended config option
-// extends: "@istanbuljs/nyc-config-typescript",
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const baseConfig = require("@istanbuljs/nyc-config-typescript");
-// Recommended modules, loading them here to speed up NYC init
-// and minimize risk of race condition
-// import 'ts-node/register';
-// import 'source-map-support/register';
 
 // Linux: prevent a weird NPE when mocha on Linux requires the window size from the TTY
 // Since we are not running in a tty environment, we just implement he method statically
@@ -29,39 +16,7 @@ if (!tty.getWindowSize) {
 }
 
 export async function run(): Promise<void> {
-    let nyc;
     const testsRoot = path.resolve(__dirname, '..');
-    if (enableCoverage) {
-        // Setup coverage pre-test, including post-test hook to report
-        nyc = new NYC({
-            ...baseConfig,
-            cwd: path.join(__dirname, '..', '..', '..'),
-            reporter: ['text', 'lcov'],
-            all: true,
-            silent: false,
-            instrument: true,
-            hookRequire: true,
-            hookRunInContext: true,
-            hookRunInThisContext: true,
-            include: ["out/**/*.js"],
-            exclude: ["out/test/**",
-                "out/gdbProxy.js",
-                "out/gdbProxyWinUAE.js"],
-        });
-        await nyc.wrap();
-
-        // Check the modules already loaded and warn in case of race condition
-        // (ideally, at this point the require cache should only contain one file - this module)
-        const myFilesRegex = /vscode-recall\/out/;
-        const filterFn = myFilesRegex.test.bind(myFilesRegex);
-        if (Object.keys(require.cache).filter(filterFn).length > 1) {
-            console.warn('NYC initialized after modules were loaded', Object.keys(require.cache).filter(filterFn));
-        }
-
-        // Debug which files will be included/excluded
-        // console.log('Glob verification', await nyc.exclude.glob(nyc.cwd));
-        await nyc.createTempDirectory();
-    }
     // Create the mocha test
     const mocha = new Mocha({
         ui: 'bdd',
@@ -81,9 +36,6 @@ export async function run(): Promise<void> {
     files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
     const failures: number = await new Promise(resolve => mocha.run(resolve));
-    if (enableCoverage) {
-        await nyc.writeCoverageFile();
-    }
     if (failures > 0) {
         throw new Error(`${failures} tests failed.`);
     }
