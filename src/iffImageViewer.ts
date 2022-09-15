94611a1a2f0872ef1120cf279621ed4df940c0c7
Paul Raingeard <paul.raingeard@gmail.com>
2022-09-15 08:32:52 +0200

    fix(iff): image view title fix

--- src/iffImageViewer.ts
+++ src/iffImageViewer.ts
@@ -120,7 +120,7 @@ export class IFFViewerPanel {
                 <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: ; script-src 'nonce-${nonce}'; connect-src vscode-resource:">
 
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Cat Coding</title>
+                <title>IFF image viewer</title>
             </head>
             <body>
                 <canvas id="iff_canvas"></canvas>
